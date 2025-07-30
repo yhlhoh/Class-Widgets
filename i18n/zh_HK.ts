2e97ac97179a6f5b5f6bef23347ec08a8f06fdbb
baiyao105 <121741105+baiyao105@users.noreply.github.com>
2025-07-30 20:08:05 +0000

    chore(i18n): 更新翻译文件 (.ts) (Auto-Commit)
    - 触发事件: push
    - 提交哈希: 67f5d7109f18e01d56eef8f72dd4826826e05f3a
    - 工作流运行: 16632654596
    
    * 由 GitHub Actions 自动生成
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- i18n/zh_HK.ts
+++ i18n/zh_HK.ts
@@ -99,18 +99,18 @@
         <translation>距離 {cd_text} 還有</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2330"/>
-        <location filename="../main.py" line="2351"/>
+        <location filename="../main.py" line="2332"/>
+        <location filename="../main.py" line="2353"/>
         <source>预警</source>
         <translation>預警</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2353"/>
+        <location filename="../main.py" line="2355"/>
         <source>{data}预警</source>
         <translation>{data}預警</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2475"/>
+        <location filename="../main.py" line="2487"/>
         <source>{city} · 未知</source>
         <translation>{city} · 未知</translation>
     </message>
@@ -4367,25 +4367,25 @@ CSES 通用課程時間表交換文件(*.yaml)</translation>
         <translation>這是一則通知範例</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2507"/>
-        <location filename="../main.py" line="2536"/>
+        <location filename="../main.py" line="2519"/>
+        <location filename="../main.py" line="2548"/>
         <source>上课时自动隐藏</source>
         <translation>上堂時自動隱藏</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2508"/>
-        <location filename="../main.py" line="2537"/>
+        <location filename="../main.py" line="2520"/>
+        <location filename="../main.py" line="2549"/>
         <source>窗口最大化时隐藏</source>
         <translation>窗口最大化時隱藏</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2509"/>
-        <location filename="../main.py" line="2538"/>
+        <location filename="../main.py" line="2521"/>
+        <location filename="../main.py" line="2550"/>
         <source>暂时无法变更“状态”</source>
         <translation>暫時無法變更&quot;狀態&quot;</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2512"/>
+        <location filename="../main.py" line="2524"/>
         <source>您正在使用 {hide_mode_text} 模式，无法变更隐藏状态
 若变更状态，将修改隐藏模式“灵活隐藏” (您稍后可以在“设置”中更改此选项)
 您确定要隐藏组件吗?</source>
@@ -4394,19 +4394,19 @@ CSES 通用課程時間表交換文件(*.yaml)</translation>
 您確定要隱藏此組件嗎？</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2516"/>
-        <location filename="../main.py" line="2545"/>
+        <location filename="../main.py" line="2528"/>
+        <location filename="../main.py" line="2557"/>
         <source>确定</source>
         <translation>確定</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2518"/>
-        <location filename="../main.py" line="2547"/>
+        <location filename="../main.py" line="2530"/>
+        <location filename="../main.py" line="2559"/>
         <source>取消</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2541"/>
+        <location filename="../main.py" line="2553"/>
         <source>您正在使用 {hide_mode_text} 模式，无法变更隐藏状态
 若变更状态，将修改隐藏模式“灵活隐藏” (您可以在“设置”中更改此选项)
 您确定要隐藏组件吗?</source>
@@ -4415,32 +4415,32 @@ CSES 通用課程時間表交換文件(*.yaml)</translation>
 你確定要隱藏此組件嗎？</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2865"/>
+        <location filename="../main.py" line="2877"/>
         <source>Class Widgets 正在运行</source>
         <translation>Class Widgets 正在運行</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2868"/>
+        <location filename="../main.py" line="2880"/>
         <source>Class Widgets 正在运行！请勿打开多个实例，否则将会出现不可预知的问题。
 (若您需要打开多个实例，请在“设置”-&gt;“高级选项”中启用“允许程序多开”)</source>
         <translation>Class Widgets 正在運行！請勿開啟多個實例，否則可能會出現不可預知的問題。
 （若您需要開啟多個實例，請於「設定」→「進階選項」中啟用「允許程式多開」）</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2901"/>
+        <location filename="../main.py" line="2913"/>
         <source>缩放系数过大</source>
         <translation>縮放係數過大</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2902"/>
+        <location filename="../main.py" line="2914"/>
         <source>当前缩放系数为 {scale_factor}%，可能导致显示异常。
 建议将缩放系数设置为 100% 到 180% 之间。</source>
         <translation>當前縮放係數為 {scale_factor}%，可能導致顯示異常。
 建議將縮放係數設定於 100% 至 180% 之間。</translation>
     </message>
     <message>
-        <location filename="../main.py" line="2869"/>
-        <location filename="../main.py" line="2903"/>
+        <location filename="../main.py" line="2881"/>
+        <location filename="../main.py" line="2915"/>
         <source>好</source>
         <translation>好</translation>
     </message>
